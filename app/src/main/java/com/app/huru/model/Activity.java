955c825f17cc38bb1f232d61297dfc52493d1c20
rgiaquinto <raphaelgiaquinto@gmail.com>
2020-01-17 11:35:02 +0100

    [FEAT] Implémentation des attributs et getters/setters de la classe Activity

--- app/src/main/java/com/app/huru/model/Activity.java
+++ app/src/main/java/com/app/huru/model/Activity.java
@@ -3,4 +3,24 @@ package com.app.huru.model;
  * Représente une activité proposée à réaliser
  * */
 public class Activity {
+
+    private String activityName;
+
+    public Activity(){
+        //constructeur vide
+    }
+
+    public Activity(String activityName){
+        this.activityName = activityName;
+    }
+
+    public String getActivityName(){
+        return this.activityName;
+    }
+
+    public void setActivityName(String activityName){
+        this.activityName = activityName;
+    }
+
+
 }
