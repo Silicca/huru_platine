066143d7401566012f5d6ce948e1efc951e024b0
rgiaquinto <raphaelgiaquinto@gmail.com>
2020-01-17 11:40:31 +0100

    [FEAT] Implémentation des attributs et getters/setters de la classe Mood

--- app/src/main/java/com/app/huru/model/Mood.java
+++ app/src/main/java/com/app/huru/model/Mood.java
@@ -3,4 +3,23 @@ package com.app.huru.model;
  * Représente une humeur dans l'application
  * */
 public class Mood {
+
+    private String moodName;
+
+    public Mood(){
+        //constructeur vide
+    }
+
+    public Mood(String moodName){
+        this.moodName = moodName;
+    }
+
+    public String getMoodName(){
+        return this.moodName;
+    }
+
+    public void setMoodName(String moodName){
+        this.moodName = moodName;
+    }
+
 }
