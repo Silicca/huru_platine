b74a77e8f5d70fda351fd748a4cd31885b69c3ed
rgiaquinto <raphaelgiaquinto@gmail.com>
2020-01-17 11:36:53 +0100

    [FEAT] Implémentation des attributs et getters/setters de la classe Hobbie

--- app/src/main/java/com/app/huru/model/Hobbie.java
+++ app/src/main/java/com/app/huru/model/Hobbie.java
@@ -3,4 +3,23 @@ package com.app.huru.model;
  * Représente un hobbie de l'utilisateur
  * */
 public class Hobbie {
+
+    private String hobbieName;
+
+    public Hobbie(){
+        //constructeur vide
+    }
+
+    public Hobbie(String hobbieName){
+        this.hobbieName = hobbieName;
+    }
+
+    public String getActivityName(){
+        return this.hobbieName;
+    }
+
+    public void setActivityName(String hobbieName){
+        this.hobbieName = hobbieName;
+    }
+
 }
