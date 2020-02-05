832a9593ee39af33de37a7143c8d764cd12a123c
Raphael <raphaelgiaquinto@gmail.com>
2020-02-05 17:32:28 +0100

    [FEAT] Mise en place de l'activité affichant les détails d'une note et permettant sa modification ou suppression

---
+++ app/src/main/java/com/app/huru/activity/NoteDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.app.huru.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoteDetailsActivity extends AppCompatActivity implements ActivityGUI {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setupGUI() {
+
+    }
+}
