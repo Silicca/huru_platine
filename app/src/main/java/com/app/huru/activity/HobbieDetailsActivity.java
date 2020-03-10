c76716dce562f845ce51062101210abb1c83f768
Raphael <raphaelgiaquinto@gmail.com>
2020-03-10 13:52:04 +0100

    [FEAT] Mise en place d'un carrousel de sélection de l'humeur pour les hobbies

--- app/src/main/java/com/app/huru/activity/HobbieDetailsActivity.java
+++ app/src/main/java/com/app/huru/activity/HobbieDetailsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,20 +12,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.huru.R;
 import com.app.huru.model.Hobbie;
+import com.app.huru.model.Mood;
 import com.app.huru.service.HobbieService;
+import com.app.huru.service.MoodService;
+import com.app.huru.tools.DrawableMoodLoader;
+
+import java.util.List;
+
 /**
  * Activité affichant les détails d'un hobbie
  * */
 public class HobbieDetailsActivity extends AppCompatActivity implements ActivityGUI {
 
     private HobbieService hobbieService;
+    private MoodService moodService;
 
     private Button modifyHobbieButton;
     private Button removeHobbieButton;
+    private Button shiftLeftButton;
+    private Button shiftRightButton;
+    private ImageView selectMoodImage;
+    private TextView selectMoodText;
 
     private TextView hobbieName;
 
     private Hobbie hobbie;
+
+    private int selectMoodIndex;
+    private List<Mood> moods;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,20 +47,33 @@ public class HobbieDetailsActivity extends AppCompatActivity implements Activity
         setContentView(R.layout.hobbie_details_activity_layout);
 
         this.hobbieService = new HobbieService(getApplicationContext());
+        this.moodService = new MoodService(getApplicationContext());
 
         int id = getIntent().getExtras().getInt("hobbieId");
+
         this.hobbie = this.hobbieService.getHobbie(id);
+
+        this.moods = this.moodService.getAllMoods();
+
+        this.selectMoodIndex = 0;
 
         this.setupGUI();
     }
 
     @Override
     public void setupGUI() {
+
         this.modifyHobbieButton = findViewById(R.id.modifyHobbieButton);
         this.removeHobbieButton = findViewById(R.id.removeHobbieButton);
-        this.hobbieName = findViewById(R.id.hobbieName);
+        this.shiftLeftButton = findViewById(R.id.shiftLeftButton);
+        this.shiftRightButton = findViewById(R.id.shiftRightButton);
 
+        this.selectMoodImage = findViewById(R.id.selectMoodImage);
+        this.selectMoodText = findViewById(R.id.selectMoodText);
+
+        this.hobbieName = findViewById(R.id.hobbieName);
         this.hobbieName.setText(this.hobbie.getName());
+
         this.modifyHobbieButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,6 +85,24 @@ public class HobbieDetailsActivity extends AppCompatActivity implements Activity
             @Override
             public void onClick(View v) {
                 removeHobbie();
+            }
+        });
+
+        this.shiftLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectMoodIndex --;
+                selectMood();
+            }
+        });
+
+        this.shiftRightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectMoodIndex ++;
+                selectMood();
             }
         });
     }
@@ -79,5 +125,19 @@ public class HobbieDetailsActivity extends AppCompatActivity implements Activity
         this.hobbieService.removeHobbie(this.hobbie.getId());
 
         finish();
+    }
+
+    private void selectMood(){
+
+        if(this.selectMoodIndex < 0){
+            this.selectMoodIndex = this.moods.size()-1;
+        }
+
+        if(this.selectMoodIndex == this.moods.size()){
+            this.selectMoodIndex = 0;
+        }
+
+        this.selectMoodImage.setImageResource(DrawableMoodLoader.load(this.moods.get(this.selectMoodIndex)));
+        this.selectMoodText.setText(this.moods.get(this.selectMoodIndex).getMoodName());
     }
 }
